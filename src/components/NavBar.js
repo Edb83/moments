1dc4f302e6a592238afc1c9e6adf76008ac81772
Edb83 <ed3radley@gmail.com>
2022-03-14 15:24:37 +0000

    Add NavBar icons and logout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,61 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
-
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>
-  const loggedOutIcons =
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="fas fa-plus-square"></i>Add post
+    </NavLink>
+  );
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
     <>
       <NavLink
         className={styles.NavLink}
@@ -26,17 +74,17 @@ const NavBar = () => {
         <i className="fas fa-user-plus"></i>Sign up
       </NavLink>
     </>
+  );
 
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-        
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -49,7 +97,6 @@ const NavBar = () => {
               <i className="fas fa-home"></i>Home
             </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
-
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -22,6 +22,7 @@ export const CurrentUserProvider = ({ children }) => {
     }
   };
 
+
   useEffect(() => {
     handleMount();
   }, []);
